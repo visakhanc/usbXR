fd35bfa2a45c6c234c3c489ae9c17f237f61cc53
Visakhan C <visakhanc@gmail.com>
2017-11-22 01:06:05 +0530

    Readme update

--- applications/SensorReceive/firmware/usbconfig.h
+++ applications/SensorReceive/firmware/usbconfig.h
@@ -249,8 +249,8 @@ section at the end of this file).
  * obdev's free shared VID/PID pair. See the file USB-IDs-for-free.txt for
  * details.
  */
-#define USB_CFG_DEVICE_NAME     'R', 'e', 'm', 'o', 't', 'e', 'B', 'o', 'o', 't'
-#define USB_CFG_DEVICE_NAME_LEN  10
+#define USB_CFG_DEVICE_NAME     'u', 's', 'b', 'X', 'R'
+#define USB_CFG_DEVICE_NAME_LEN  5
 /* Same as above for the device name. If you don't want a device name, undefine
  * the macros. See the file USB-IDs-for-free.txt before you assign a name if
  * you use a shared VID/PID.
